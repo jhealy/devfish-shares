269499cbcdedb09c10120e3451fee36462582d69
Joe Healy <josephehealy@hotmail.com>
2018-12-17 09:30:35 -0500

    .net version of translate console added

---
+++ TranslateV3ConsoleNET/TranslateV3ConsoleNET/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranslateV3ConsoleNET
+{
+    public static class Constants
+    {
+        public static string TRANSLATE_KEY {get;} = @"PUT YOUR KEY HERE";
+    }
+}
---
+++ TranslateV3ConsoleNET/TranslateV3ConsoleNET/DictionaryLookup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+// NOTE: Install the Newtonsoft.Json NuGet package.
+using Newtonsoft.Json;
+
+namespace TranslateV3ConsoleNET
+{
+    public static class DictionaryLookup
+    {
+        static string host = "https://api.cognitive.microsofttranslator.com";
+        static string path = "/dictionary/lookup?api-version=3.0";
+        // Translate from English to French.
+        static string params_ = "&from=en&to=fr";
+
+        static string uri = host + path + params_;
+
+        // NOTE: Replace this example key with a valid subscription key.
+        static string key = Constants.TRANSLATE_KEY;
+
+        static string text = "great";
+
+        public async static Task<bool> DoLookup()
+        {
+            Console.WriteLine("--- Dictionary Lookup ---");
+            System.Object[] body = new System.Object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+
+                Console.OutputEncoding = UnicodeEncoding.UTF8;
+                Console.WriteLine(result);
+
+                return true;
+            }
+        }
+    }
+}
