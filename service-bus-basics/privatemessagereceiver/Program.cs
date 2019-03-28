139ba48da9fbc17066194f0d6977efe9bfde1706
Joe Healy <josephehealy@hotmail.com>
2019-03-28 17:49:13 -0400

    service bus basics initial checkin

---
+++ service-bus-basics/privatemessagereceiver/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace privatemessagereceiver
+{
+    class Program
+    {
+
+        const string m_ServiceBusConnectionString = @"insert-connection-string-here";
+        const string m_QueueName = "salesmessages";
+        static IQueueClient m_queueClient;
+
+        static void Main(string[] args)
+        {
+
+            ReceiveSalesMessageAsync().GetAwaiter().GetResult();
+
+        }
+
+        static async Task ReceiveSalesMessageAsync()
+        {
+
+            // Create a Queue Client here
+            m_queueClient = new QueueClient(m_ServiceBusConnectionString, m_QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            RegisterMessageHandler();
+        
+            Console.Read();
+
+            // Close the queue here
+            await m_queueClient.CloseAsync();
+
+        }
+
+        static void RegisterMessageHandler()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            m_queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            long sentTicks;
+            if (long.TryParse(Encoding.UTF8.GetString(message.Body), out sentTicks))
+            {
+                long nowTicks = DateTime.Now.Ticks;
+                int ticksToMS = 10000;
+                int mstofetch = (int)((nowTicks - sentTicks)/ticksToMS);
+                double secstofetch = mstofetch / 1000.00;
+
+                Console.WriteLine($"{(int)(nowTicks/ticksToMS)}:now - {(int)(sentTicks/ticksToMS)}:sent = {mstofetch}:ms {secstofetch}:seconds");
+            }
+            else
+            {
+                Console.WriteLine("Message was not in millisecond format.  Error.");
+            }
+
+            await m_queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }   
+    }
+}
---
+++ service-bus-basics/privatemessagesender/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+
+        const string m_ServiceBusConnectionString = @"insert-connection-string-here";
+        const string m_QueueName = "salesmessages";
+        static IQueueClient m_queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            // Create a Queue Client here
+            m_queueClient = new QueueClient(m_ServiceBusConnectionString, m_QueueName);
+
+            // Send messages.
+            try
+            {
+                for ( int ii=0; ii<10; ii++ )
+                {
+                    // Create and send a message here
+                    // string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";              
+                    long nowTicks = DateTime.Now.Ticks;
+                    string messageBody = nowTicks.ToString();
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    Console.WriteLine($"Sending message: {messageBody}");
+                    await m_queueClient.SendAsync(message);
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the queue here
+            await m_queueClient.CloseAsync();
+        }
+    }
+}
